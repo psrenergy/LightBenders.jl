9cc77fe01241e46468ca8075b8497abfd27f79b8
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-11-08 00:12:48 -0300

    Project updates

--- revise.bat
+++ revise.bat
@@ -3,4 +3,4 @@
 SET BASEPATH=%~dp0
 SET REVISE_PATH="%BASEPATH%\revise"
 
-%JULIA_194% --color=yes --project=%REVISE_PATH% --load=%REVISE_PATH%\revise_load_script.jl
+%JULIA_194% --project=%REVISE_PATH% --load=%REVISE_PATH%\revise_load_script.jl
