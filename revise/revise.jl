449b90e648fb02f61f517f695a42355f041283d7
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-11-25 14:02:25 -0300

    Project updates

--- revise/revise.jl
+++ revise/revise.jl
@@ -8,6 +8,6 @@ Pkg.instantiate()
 
 using LightBenders
 @info("""
-This session is using LightBenders with Revise.jl.
+This session is using LightBenders.jl with Revise.jl.
 For more information visit https://timholy.github.io/Revise.jl/stable/.
 """)
