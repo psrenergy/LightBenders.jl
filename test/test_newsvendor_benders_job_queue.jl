08cb97c5022f603a2f59c61cb2865250d5434e49
Raphael Sampaio <raphaelaraujosampaio@gmail.com>
2024-11-25 13:13:35 -0300

    Update test_newsvendor_benders_job_queue.jl

--- test/test_newsvendor_benders_job_queue.jl
+++ test/test_newsvendor_benders_job_queue.jl
@@ -5,6 +5,4 @@ using JobQueueMPI.MPI
 project_dir = dirname(Base.active_project())
 parallel_script = joinpath(@__DIR__, "script_newsvendor_benders_job_queue.jl")
 
-mpiexec(
-    exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project=$(project_dir) $(parallel_script)`),
-)
+mpiexec(exe -> run(`$exe -n 3 $(Base.julia_cmd()) --project=$(project_dir) $(parallel_script)`))
