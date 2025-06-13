76134a5d6992b5ad94ff1bc55ae2a33fa44afd6a
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-06-12 21:30:57 -0300

    update job queue

--- src/training_strategies/benders_job_queue.jl
+++ src/training_strategies/benders_job_queue.jl
@@ -41,7 +41,6 @@ function job_queue_benders_train(;
 
     controller = JQM.Controller(JQM.num_workers())
     progress = BendersTrainingIterationsLog(policy_training_options)
-    pool = initialize_cut_pool(policy_training_options)
     iteration_pool = initialize_cut_pool(policy_training_options)
     state = Float64[] # State variables are only stored for the first stage that does not vary per scenario
     state_cache = StateCache()
@@ -50,7 +49,7 @@ function job_queue_benders_train(;
     stage = 1
     state_variables_model = state_variables_builder(inputs, stage)
     first_stage_model = first_stage_builder(state_variables_model, inputs)
-    create_epigraph_variables!(first_stage_model, policy_training_options)
+    create_epigraph_variables!(first_stage_model, iteration_pool[1], policy_training_options)
 
     # second stage model (here in the controller, only used for checking if the states match)
     stage = 2
@@ -65,18 +64,15 @@ function job_queue_benders_train(;
         start_iteration!(progress)
         t = 1
 
-        add_all_cuts!(first_stage_model, iteration_pool[t], policy_training_options)
-        store_retry_data(first_stage_model, policy_training_options)
-        optimize_with_retry(first_stage_model)
-        treat_termination_status(first_stage_model, policy_training_options, t, progress.current_iteration)
+        initialize_cuts!(first_stage_model, iteration_pool[t], policy_training_options)
+        optimize_first_stage(first_stage_model, iteration_pool[t], policy_training_options, progress)
         state = get_state(first_stage_model)
         future_cost = get_future_cost(first_stage_model, policy_training_options)
         progress.LB[progress.current_iteration] += JuMP.objective_value(first_stage_model)
         progress.UB[progress.current_iteration] += JuMP.objective_value(first_stage_model) - future_cost
-        iteration_pool = initialize_cut_pool(policy_training_options)
 
         t = 2
-        local_pools = LocalCutPool()
+        local_pools = LocalCutPool(policy_training_options)
         for s in 1:policy_training_options.num_scenarios
             message = SecondStageMessage(progress.current_iteration, s, state)
             JQM.add_job_to_queue!(controller, message)
@@ -106,8 +102,8 @@ function job_queue_benders_train(;
         # Store the (stage, scenario) cut(s) in a persitent pool.
         # Cuts here can be following the single cut strategy or 
         # the multi cut strategy
-        store_cut!(pool, local_pools, state, policy_training_options, t)
         store_cut!(iteration_pool, local_pools, state, policy_training_options, t)
+        reset_cuts!(first_stage_model, iteration_pool[1])
         progress.UB[progress.current_iteration] += second_stage_upper_bound_contribution(
             policy_training_options, local_pools.obj,
         )
@@ -124,7 +120,7 @@ function job_queue_benders_train(;
 
     return Policy(
         progress = progress,
-        pool = pool,
+        pool = iteration_pool,
         states = state,
         policy_training_options = policy_training_options,
     )
